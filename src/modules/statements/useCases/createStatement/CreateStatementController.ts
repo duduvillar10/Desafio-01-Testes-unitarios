4dd018112b1ed3a00f894b85e709b97430ff5a32
duduvillar10 <duduvillar10io@hotmail.com>
2022-04-12 16:29:39 -0300

    feat: transfer statement

--- src/modules/statements/useCases/createStatement/CreateStatementController.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -1,19 +1,21 @@
-import { Request, Response } from 'express';
-import { container } from 'tsyringe';
+import { Request, Response } from "express";
+import { container } from "tsyringe";
 
-import { CreateStatementUseCase } from './CreateStatementUseCase';
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 enum OperationType {
-  DEPOSIT = 'deposit',
-  WITHDRAW = 'withdraw',
+  DEPOSIT = "deposit",
+  WITHDRAW = "withdraw",
+  TRANSFER = "transfer",
 }
 
 export class CreateStatementController {
   async execute(request: Request, response: Response) {
     const { id: user_id } = request.user;
     const { amount, description } = request.body;
+    const { sender_id } = request.params;
 
-    const splittedPath = request.originalUrl.split('/')
+    const splittedPath = request.originalUrl.split("/");
     const type = splittedPath[splittedPath.length - 1] as OperationType;
 
     const createStatement = container.resolve(CreateStatementUseCase);
@@ -21,8 +23,9 @@ export class CreateStatementController {
     const statement = await createStatement.execute({
       user_id,
       type,
+      sender_id,
       amount,
-      description
+      description,
     });
 
     return response.status(201).json(statement);
